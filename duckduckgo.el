417b0042159f62146b8c46475b36d1f6aabed8d7
Victor Deryagin <vderyagin@gmail.com>
2026-01-10 01:34:28 +0200

    public domain

--- duckduckgo.el
+++ duckduckgo.el
@@ -1,27 +1,16 @@
 ;;; duckduckgo.el --- An interface for DuckDuckGo web search engine -*- lexical-binding: t -*-
 
-;; Copyright (C) 2015-2022 Victor Deryagin
-
 ;; Author: Victor Deryagin <vderyagin@gmail.com>
 ;; Maintainer: Victor Deryagin <vderyagin@gmail.com>
 ;; Created: 25 Feb 2015
 ;; Version: 0.4.2
+
 ;; Package-Requires: ((consult))
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2, or (at
-;; your option) any later version.
+;; This file is not part of GNU Emacs.
 
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; This is free and unencumbered software released into the public domain.
+;; For more information, please refer to <http://unlicense.org/>
 
 ;;; Commentary:
 
